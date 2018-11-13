c306fc172c76cd5c3962ce83efb70db03bd00057
Alberto Iannaccone <alby.ianna@gmail.com>
2018-11-13 19:43:10 +0100

    move tank almost decently

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Public/TankMovementComponent.h"
+#include "Public/TankTrack.h"
 
-void UTankMovementComponent::IntendMoveForward(float Throw) {
-	UE_LOG(LogTemp, Warning, TEXT("IntendMoveForward is throwing: %f"), Throw)
+void UTankMovementComponent::Initialize(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) {
+		return;
+	}
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
 }
 
+void UTankMovementComponent::IntendMoveForward(float Throw) {
+	if (!LeftTrack || !RightTrack) {
+		return;
+	}
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
 
+void UTankMovementComponent::IntendMoveRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) {
+		return;
+	}
+	LeftTrack->SetThrottle(-Throw * 5);
+	RightTrack->SetThrottle(Throw * 5);
+}
