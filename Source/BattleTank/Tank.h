cc2a66174ac2de15682910b6195229d39a9e2631
Alberto Iannaccone <alby.ianna@gmail.com>
2019-02-22 00:35:05 +0100

    add health bar

--- Source/BattleTank/Tank.h
+++ Source/BattleTank/Tank.h
@@ -10,12 +10,18 @@ UCLASS()
 class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
+	
+public:
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+	// Return current health as a prercentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 
 private:
 	// Sets default values for this pawn's properties
 	ATank();
-
-	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 StartingHealth = 100;
